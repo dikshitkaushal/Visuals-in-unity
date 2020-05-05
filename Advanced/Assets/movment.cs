4d3928df8283c692e0a6d503c7da16b333525e07
Dikshit Kaushal <47450978+dikshitkaushal@users.noreply.github.com>
2020-05-05 10:49:31 +0530

    apply_fog

---
+++ Advanced/Assets/movment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class movment : MonoBehaviour
+{
+    private float speed=2f;
+    float movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*transform.Translate(0,0,speed * Time.deltaTime);*/
+        movement += speed * Time.deltaTime;
+        transform.position = new Vector3(0, 1, movement);
+    }
+}
